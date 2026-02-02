622b07a233b4464ebfe87724bc6962ef7875163e
IXOlex <emerl2104@live.fr>
2026-02-02 10:23:03 +0100

    Commit aprés séssion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import Card from "../components/Card/Card";
+import "./Home.css";
+
 function Home() {
-  return <h1>HOME</h1>;
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/properties")
+      .then((response) => response.json())
+      .then((data) => setProperties(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <main>
+      <section className="cards">
+        {properties.map((property) => (
+          <Card
+            key={property.id}   // clé unique pour chaque carte
+            id={property.id}
+            title={property.title}
+            cover={property.cover}
+          />
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default Home;
