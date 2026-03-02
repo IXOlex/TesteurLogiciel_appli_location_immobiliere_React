50d73672de97e7f7f7b591a242a9c3147a55d716
IXOlex <emerl2104@live.fr>
2026-03-02 10:17:58 +0100

    Update Footer path img

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <img src="src/images/LOGO.svg" alt="Kasa" />
+      <img src="./src/images/LOGO.svg" alt="Kasa" />
       <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
